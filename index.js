f4b83b08a9db8783e412ef9ef645840a49cb5f6c
NozoDev <kevinroj326@gmail.com>
2024-04-29 20:59:19 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+let msn = "Hola mundo";
+const hero = {
+    name: 'iron maiden',
+    edad: 45
+};
+console.log(hero);
---
+++ index.ts
@@ -0,0 +1,9 @@
+let msn:string = "Hola mundo"
+
+const hero = {
+    name: 'iron maiden',
+    edad: 45
+}
+
+
+console.log(hero)
