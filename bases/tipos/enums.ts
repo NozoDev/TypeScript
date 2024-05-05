58275f89fee7170ee794c77804ec226891b046f8
NozoDev <kevinroj326@gmail.com>
2024-05-04 21:43:46 -0500

    void

---
+++ bases/tipos/enums.ts
@@ -0,0 +1,12 @@
+(() => {
+    enum AudioLevel {
+        min,  //0
+        medium, //1
+        max //2
+    } 
+
+    const currentAudio = AudioLevel.medium
+
+    console.log(currentAudio)
+    console.log(AudioLevel)
+}) ()
---
+++ bases/tipos/void.js
@@ -0,0 +1,12 @@
+"use strict";
+(() => {
+    function callBatman() {
+        return;
+    }
+    const callSuperman = () => {
+        return;
+    };
+    const a = callBatman();
+    console.log(a);
+})();
+// void es vacio.. no hayu nada
